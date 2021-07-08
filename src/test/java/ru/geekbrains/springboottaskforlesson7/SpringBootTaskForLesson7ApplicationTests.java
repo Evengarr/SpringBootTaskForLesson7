f7f2e2189d35ad5cbf0e1c52457d69cc4b4d23d8
Evengarr <Ravasect@gmail.ru>
2021-07-08 21:05:28 +0300

    Initial commit

---
+++ src/test/java/ru/geekbrains/springboottaskforlesson7/SpringBootTaskForLesson7ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.springboottaskforlesson7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTaskForLesson7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
